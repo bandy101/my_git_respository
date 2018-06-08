426212b33833c447a6c405c932bf2a2f1a18aa9c
michaelzhuwb <1255381744@qq.com>
2018-06-08 15:29:06 +0800

    update sql_6-8

--- FTP/FTPMFC/SQL_operation.hpp
+++ FTP/FTPMFC/SQL_operation.hpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <stdio.h>
 #include <fstream>
+#include "../FTP/FTPClient.h"
+#define CORD_SIZE 100
 //#include <cstring>
 using namespace std;
 class MySql
@@ -12,33 +14,36 @@ class MySql
 public:
 	MySql(char *host, char *user, char *pwd, char *table, int port)
 	{
-		char *encode = "gb2312";
 		pCon = mysql_init(NULL);
-		datas = " "; error = " ";
-		if (mysql_real_connect(pCon, host, user, pwd, table, port, NULL, 0))
-		{
+		pCon_img = mysql_init(NULL);
+		datas = " "; error = " "; cord_num = 0;
+		if (mysql_real_connect(pCon, host, user, pwd, table, port, NULL, 0)&& mysql_real_connect(pCon_img, host, user, pwd, table, port, NULL, 0))
+		{	
+			//设置编码
+			encode = "SET NAMES 'gb2312'";
+			mysql_query(pCon, encode);
+			mysql_query(pCon_img, encode);
 
-			char *result = "SET NAMES 'gb2312'";
-			/*		strcat(result,encode);
-			strcat(result,"'");*/
-			mysql_query(pCon, result);
-			mysql_query(pCon, "select * from telemetry_image");
+			mysql_query(pCon, "select * from telemetry_message");
+			mysql_query(pCon_img, "select * from telemetry_image");
+
+			result_msg = mysql_store_result(pCon);
+			result_img = mysql_store_result(pCon_img);
+
 		}
 		else
 		{
+			image_name = "连接数据库失败";
 			error = "连接数据库失败！";
 		}
 	}
 	~MySql() {};
-	bool read_data_save_img(int data_row = 36)
+	bool read_data_save_img(int data_row = 42)
 	{
-
-
-		result = mysql_store_result(pCon);
 		bool rd = true;
-		while (row = mysql_fetch_row(result))
+		while (row = mysql_fetch_row(result_msg))
 		{
-
+			row_img = mysql_fetch_row(result_img);
 			for (int i = 0; i < data_row; i++)
 				try
 			{
@@ -48,7 +53,6 @@ public:
 					datas += row[i];
 					datas += ";";
 				}
-
 			}
 			catch (exception)
 			{
@@ -56,19 +60,23 @@ public:
 				datas += ",";
 			}
 			datas += "\n";
-			string image_name = row[26];
-			int pos = image_name.rfind("/");
-			unsigned long *lengths = mysql_fetch_lengths(result); //lenghs[n] 该字段长度
+			image_name = row[27];
+			name[cord_num++] = image_name; //记录imge_表名
+			unsigned long *lengths = mysql_fetch_lengths(result_img); //lenghs[n] 该字段长度
 			destIDs = (uint32_t *)malloc(lengths[2]);
-			memcpy(destIDs, row[2], lengths[2]);
-			ofstream fout(image_name.substr(pos,sizeof(image_name)+1), ios::binary);
+			memcpy(destIDs, row_img[2], lengths[2]);
+			ofstream fout(image_name.substr(9, sizeof(image_name)), ios::binary);
+			//ofstream fout(image_name, ios::binary);
 			temp = (uint32_t *)malloc(lengths[2]);
 			temp = destIDs;
 			fout.write((char*)destIDs, lengths[2]);
 			fout.close();
+			//cord_num += 1;
 
 		}
-		mysql_free_result(result);
+		mysql_free_result(result_img);
+		mysql_free_result(result_msg);
+		mysql_close(pCon_img);
 		mysql_close(pCon);
 		return rd;
 	}
@@ -78,8 +86,18 @@ public:
 	char buf[4096];
 	uint32_t *destIDs, *temp;
 	unsigned int destNum = 16000;
+	string image_name;
+	int cord_num;
+	string name[CORD_SIZE];
 private:
+	
+	FTPClient ftp;
+	char *encode;
+	MYSQL_RES *result_img;
+	MYSQL_RES *result_msg;
 	MYSQL *pCon;
+	MYSQL *pCon_img;
 	MYSQL_ROW row;
+	MYSQL_ROW row_img;
 	MYSQL_RES *result;
 };
