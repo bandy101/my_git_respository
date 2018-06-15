6668da7a344e2905b556bdb3d51f1539848dba9d
michaelzhuwb <1255381744@qq.com>
2018-06-15 17:34:56 +0800

    update althmitic

--- reset_learning_py/althmitic.py
+++ reset_learning_py/althmitic.py
@@ -29,7 +29,7 @@ def heap_sort(lists):
 def heap_fy(parent_point,length,lists): 
     k = parent_point*2+1
     temp = lists[parent_point]
-    ##K <length 
+    ##k<length   because from subscript 0 start 
     while k<length:
         #指向较大的子节点
         if (k+1)<length and lists[k]<lists[k+1]:
@@ -42,21 +42,68 @@ def heap_fy(parent_point,length,lists):
         k =2*k+1      ##   important 重点
     lists[parent_point] = temp
 
-def build_heap(lists,start):
-    
-    k = start
-    le ,ri=2*start+1, 2*start+2
-    if le<len(lists) and le>lists[start]:
-        k=le
-    if ri<len(lists) and ri>lists[start]:
-        k=ri
-    if k!=start:
-        swap(lists,k,start)
-    # else:
-    #     build_heap(lists)
+
+def _heap_sort(lists):
+    last_p = len(lists)//2-1   #subscript from 0 start
+    while last_p >0:
+        build_heap(lists,last_p)
+        last_p -=1
+    return lists
+def build_heap(lists,last_p):
+    temp = lists[last_p]
+    l,k= 2*last_p+1,2*last_p+1
+    if l>=len(lists):return
+    if (l+1)<len(lists) and lists[l]>lists[l+1]  :
+        k +=1
+    if lists[k]>temp:
+        lists[last_p] = lists[k]
+        last_p = k
+        
+    k =2*k+1
+    if k<len(lists):
+        build_heap(lists,k)
+    lists[last_p] = temp
+    # while k < len(lists):
+    #     build_heap(lists,k)
+    #     k = 2*k+1
+
+global i
+i=1
+def quicksort(lists,left,right):
+    global i
+    if  (left>=right):
+        return
+    temp = lists[left]  ##基准数
+    l,r = left+1,right
+    while l!=r:
+        while lists[r]>=temp and l<r:
+            r -=1
+        while lists[l]<=temp and l<r:
+            l +=1
+        print("l:%d,r:%d"%(l,r))
+        print("####################")
+        if l<r: 
+            # print("temp:",temp)
+            print("l:%d,r:%d"%(l,r))
+            swap(lists,l,r)
+            print(lists)
+    lists[left] = lists[l]
+    lists[l] = temp
+    print("%d:"%i,lists)
+    i +=1
+    quicksort(lists,left,l-1)
+    quicksort(lists,l+1,right)
+
+
+
+
+
 c = [3,4,5]
 # start = len(lists)//2-1
 a =[R.randint(0,100) for i in range(10)]
+
+
 b= [31, 95, 98, 40, 61, 78, 20, 94, 6, 46]
-print("origin:",a)
-print("sort:",heap_sort(a))
+print("origin:",b)
+quicksort(b,0,len(b)-1)
+print("sort:",b)
