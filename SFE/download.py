1a54ac102cadfb1a42da20ed7626a18cd744b50f
michaelzhuwb <1255381744@qq.com>
2018-09-11 17:32:40 +0800

    update

--- SFE/download.py
+++ SFE/download.py
@@ -1,7 +1,7 @@
 import requests
 import os 
 from os import path
-
+import json,time
 prex = 'http://202.105.10.126:8055/video/'
 # url = 'http://202.105.10.126:8055/video/20180911/15366527703302.mp4'
 # paths ='./video/2.mp4'
@@ -26,19 +26,47 @@ def dwd(paths,url):
     except Exception as e:
         print(e)
 def get_token(url,name='demo',pwd='demo&123'):
-    js_pwd ={
-    "clientId":"098f6bcd4621d373cade4e832627b4f6",
-    'userName':name,
-    'password':pwd
-    }
     try:
+        js_pwd ={
+            "clientId":"098f6bcd4621d373cade4e832627b4f6",
+            'userName':name,
+            'password':pwd
+        }
         res = requests.post(url,json=js_pwd,verify=False)
         res =json.loads(res.content)
         token = res['content']['token']
         return token
-    except Exception e:
+    except Exception as e:
         print(e)
 
-def get_
+def get_mp4_info(url):
+    url_qy = 'http://202.105.10.126:8055/api/v1/login/'
+    'http://202.105.10.126:8055/api/v1/smokeMessagePageQuery?\
+    provinceId=440000&cityId=441800&countyId=&tsNo=&license=\
+    &monitorBeginTime=2018-09-11 00:00:00&monitorEndTime=\
+    2018-09-11 23:59:59&pageNum=2&pageSize=30'
+    begindate= time.strftime("%Y-%m-%d", time.localtime())+' 00:00:00'
+    enddate = time.strftime("%Y-%m-%d", time.localtime())+' 23:59:59'
+    params = {
+        'provinceId':440000,
+        'cityId':441800,
+        'countyID':'',
+        'tsNo':'',
+        'monitorBeginTime':str(begindate),
+        'monitorEndTime':str(enddate),
+        'pageSize':1000,
+        'pageNum':1
+    }
+    token = get_token(url_qy)
+
+    res = requests.get(url,params=params,headers={'Authorization':'bearer  '\
+        +token},timeout=6000,verify= False)
+    print(res.url)
+    res = json.loads(res.content)
+    lists = res['content']['list']
+    print(len(lists))
 if __name__=='__main__':
-    dwd(paths)
+    # dwd(paths)
+    url_qy = 'http://202.105.10.126:8055/api/v1/login/'
+    url = 'http://202.105.10.126:8055/api/v1/smokeMessagePageQuery'
+    get_mp4_info(url)
