d84498ee7d25b6c5c4f0a20c0d4c5bceb34507ff
michaelzhuwb <1255381744@qq.com>
2018-09-12 17:21:18 +0800

    update

--- SFE/download.py
+++ SFE/download.py
@@ -5,7 +5,7 @@ import json,time
 prex = 'http://202.105.10.126:8055/video/'
 # url = 'http://202.105.10.126:8055/video/20180911/15366527703302.mp4'
 # paths ='./video/2.mp4'
-
+import zipfile
 
 def dwd(paths,url):
     try:
@@ -60,8 +60,24 @@ def get_mp4_info(url):
     res = json.loads(res.content)
     lists = res['content']['list']
     print(len(lists))
+
+def zips(startdir=None,file_news=None):
+    startdir = "./target_smoke"  #要压缩的文件夹路径
+    file_news = '.'+startdir +'.zip' # 压缩后文件夹的名字
+    print(file_news)
+    z = zipfile.ZipFile(file_news,'w',zipfile.ZIP_DEFLATED) #参数一：文件夹名（路径）
+    for dirpath, dirnames, filenames in os.walk(startdir):
+        fpath = dirpath.replace(startdir,'') #这一句很重要，不replace的话，就从根目录开始复制
+        print('fpath:',fpath)
+        fpath = fpath and fpath + os.sep or ''#这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
+        for filename in filenames:
+            z.write(os.path.join(dirpath, filename),fpath+filename)
+            print ('压缩成功')
+    z.close()
+
 if __name__=='__main__':
     # dwd(paths)
     url_qy = 'http://202.105.10.126:8055/api/v1/login/'
     url = 'http://202.105.10.126:8055/api/v1/smokeMessagePageQuery'
-    get_mp4_info(url)
+    # get_mp4_info(url)
+    zips()
