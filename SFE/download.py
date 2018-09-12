dbe323f05fd90176b32d23a8075c7c3b2788eefd
michaelzhuwb <1255381744@qq.com>
2018-09-12 17:45:33 +0800

    update

--- SFE/download.py
+++ SFE/download.py
@@ -63,16 +63,21 @@ def get_mp4_info(url):
 
 def zips(startdir=None,file_news=None):
     startdir = "./target_smoke"  #要压缩的文件夹路径
-    file_news = '.'+startdir +'.zip' # 压缩后文件夹的名字
+    times = time.strftime('%Y-%m-%d',time.localtime())
+    fpath = 'H:/分类任务/清远黑烟/清远平台/'+times
+    file_news = fpath +'.zip' # 压缩后文件夹的名字
     print(file_news)
     z = zipfile.ZipFile(file_news,'w',zipfile.ZIP_DEFLATED) #参数一：文件夹名（路径）
     for dirpath, dirnames, filenames in os.walk(startdir):
-        fpath = dirpath.replace(startdir,'') #这一句很重要，不replace的话，就从根目录开始复制
-        print('fpath:',fpath)
-        fpath = fpath and fpath + os.sep or ''#这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
+        pp = path.join(fpath, path.basename(dirpath))
+        print(pp)
+        if not path.exists(pp):os.makedirs(pp)
+        # fpath = dirpath.replace(startdir,'') #这一句很重要，不replace的话，就从根目录开始复制
+        print('dirpath:',dirpath)
+        # fpath = fpath and fpath + os.sep or ''#这句话理解我也点郁闷，实现当前文件夹以及包含的所有文件的压缩
         for filename in filenames:
-            z.write(os.path.join(dirpath, filename),fpath+filename)
-            print ('压缩成功')
+            z.write(path.join(dirpath,filename))
+            print ('压缩成功,',path.join(dirpath,filename))
     z.close()
 
 if __name__=='__main__':
