348e66fdddf0eddfcc7099cd644275703f53dcd3
michaelzhuwb <1255381744@qq.com>
2018-11-16 18:16:22 +0800

    update

--- SFE/xinxiang.py
+++ SFE/xinxiang.py
@@ -49,8 +49,11 @@ def get_token(url,name='demo',pwd='demo&123'):
 
 def get_mp4_info(url):
     url_xx = 'http://117.158.91.105:6153/api/v1/login/'
-    begindate= time.strftime("%Y-%m-%d", time.localtime())+' 00:00:00'
-    enddate = time.strftime("%Y-%m-%d", time.localtime())+' 23:59:59'
+    # begindate= time.strftime("%Y-%m-%d", time.localtime())+' 00:00:00'
+    # enddate = time.strftime("%Y-%m-%d", time.localtime())+' 23:59:59'
+    begindate = '2018-04-01 00:00:00'
+    enddate = '2018-12-01 00:00:00'
+
     print(f'date-->-:{begindate}<-->{enddate}')
     params = {
         'provinceId':410000,
@@ -74,7 +77,6 @@ def get_mp4_info(url):
         +token},timeout=6000,verify= False)
         re = json.loads(re.content)['content']
         info.append((re['url'],re['tsNo']))
-    print(len(lists))
     return lists,info,len(lists)
 def smoke_move(ID):
     if ID in [None,'',' ']:
@@ -135,30 +137,43 @@ if __name__=='__main__':
     }
     url_qy = 'http://117.158.91.105:6153/api/v1/login/'
     url = 'http://117.158.91.105:6153/api/v1/smokeMessagePageQuery'
-    pool = ThreadPoolExecutor(max_workers=4)
-    l,info,total_video_num= get_mp4_info(url)
-    p = []#名字
-    ur = []
-    for _,(u,tsno)in zip(l,info):
-        time_name = time.strftime('%Y-%m-%d %H.%M.%S',time.localtime(int(str(_['monitorTime'])[0:10])))
-        site = TSNO[tsno]
-        p.append(('./video/'+site+'/'+time_name+'-'+_['id']+'.mp4'))
-        ur.append('http://117.158.91.105:6153/video/'+u)
+    # pool = ThreadPoolExecutor(max_workers=4)
+    # l,info,total_video_num= get_mp4_info(url)
+    # p = []#名字
+    # ur = []
+    # for _,(u,tsno)in zip(l,info):
+    #     time_name = time.strftime('%Y-%m-%d %H.%M.%S',time.localtime(int(str(_['monitorTime'])[0:10])))
+    #     site = TSNO[tsno]
+    #     p.append(('./video/'+site+'/'+time_name+'-'+_['id']+'.mp4'))
+    #     ur.append('http://117.158.91.105:6153/video/'+u)
 
-    all_task = [pool.submit(dwd,p,ur) for p,ur in zip(p,ur)]
-    wait(all_task,return_when=ALL_COMPLETED)
-    print('today smoke’s num->total_video_num:',total_video_num)
-    x =input('请输入ID号:')
-    while True:
-        if x =='q' or x =='quit':
-            break
-        Y =smoke_move(x)
-        print('退出输入q or quit!')
-        if Y:
-            print('目标ID移动成功')
-            x =input('请输入新ID:')
-            # break
-        else:
-            x =input('找不到该ID,请输入正确ID:') 
+    # all_task = [pool.submit(dwd,p,ur) for p,ur in zip(p,ur)]
+    # wait(all_task,return_when=ALL_COMPLETED)
+    # print('today smoke’s num->total_video_num:',total_video_num)
+    # x =input('请输入ID号:')
+    # while True:
+    #     if x =='q' or x =='quit':
+    #         break
+    #     Y =smoke_move(x)
+    #     print('退出输入q or quit!')
+    #     if Y:
+    #         print('目标ID移动成功')
+    #         x =input('请输入新ID:')
+    #         # break
+    #     else:
+    #         x =input('找不到该ID,请输入正确ID:') 
 
-    zips()
+    # zips()
+    lists ,_,list_num = get_mp4_info(url)
+    for _ in lists:
+        car_number = _['license']
+        car_color =_['licenseTypeStr']
+        grab_time = _['monitorTimeStr']
+        grab_point = _['name']
+        ringelman = _['ringelman']
+        targit = str(car_number)+'\t'+car_color+'\t'+str(grab_time)+'\t'+str(grab_point)+'\t'+str(ringelman)
+        with open('data.txt',mode='a') as f:
+            f.write(targit)
+            f.write('\n')
+        # print(car_number,car_color,grab_time,grab_point,ringelman)
+    print(list_num)
