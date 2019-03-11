3022cddcf9e0b79c6010b44b4cdda1fb48765f66
michaelwbzhu <1255381744@qq.com>
2019-03-11 18:14:30 +0800

    update

--- OWORLD/worldThree/common/print_shd.py
+++ OWORLD/worldThree/common/print_shd.py
@@ -111,49 +111,214 @@ def printPdfShd(request):
     pageWidth = 21.7     
     pageHeight = 28.0
     response = HttpResponse(content_type='application/pdf')   
-
+    global pdf
     pdf = canvas.Canvas(response) # 实例化pdf
 
     # 设置pdf样式 
 
     # 1 inch = 2.54cm  1cm = 28.346 dpi
     pdf.setPageSize((pageWidth*cm,pageHeight*cm)) # 尺寸
-    pdf.setFont('song',9) # 设置字体
+    pdf.setFont('song',25) # 设置字体
+  
     
     # 加载图片 background
     two_dimensional_code_path = L2['qr_code']
     file_path = r"/home/webroot/data/lwerp/open/background/total.png"
     pdf.drawImage(file_path,0,0,pageWidth*cm,pageHeight*cm)                 # 背景
-    pdf.drawImage(two_dimensional_code_path,17.3*cm,25.28*cm,2.6*cm,2.6*cm) # 二维码 
+    pdf.drawImage(two_dimensional_code_path,17.61*cm,24.3*cm,2.82*cm,2.82*cm) # 二维码 
 
     # 加载内容
 
-    amount = 15                     # 每页显示的数目 (四联二等分:5, 四联整张纸:15)
-    pages = len(data)//amount + 1         # 页数
+    amounT = 3                     # 每页显示的数目 (四联二等分:5, 四联整张纸:15)
+    pages = len(data)//amounT + 1         # 页数
     # print data
 
+    tel = L2['sup_tel']                   # 电话
+    usr_name = L2['usr_name']             # 制单人
+    cg_usr_name = L2['cg_usr_name']       # 采购员
+    shd_no = L2['shd_no']                 # 送货单号
+    cgzt   = L2['cgzt']                   # 采购主体
+    zt = L2['sup_name']                           # 送货单最上面大字
+    project_name = L2['proj_name']     # 收货项目
+    sh_date = L2['sh_date']                   # 送货日期
+    all_amount  = 0.                       # 合计金额
+
+
+    pdf.drawString(21,755,str(zt))    # 送货单最上面大字
+    pdf.setFont('song',9) # 设置字体  
+
+    pdf.drawString(135,21.5,str(cg_usr_name)) # 采购员
+    pdf.drawString(55,21.5,str(usr_name))    # 制单人
+    pdf.drawString(361,727,str(tel))    # 电话
+
+    pdf.drawString(65,727,str(shd_no))    # 送货单号
+    pdf.drawString(65,710.5,str(cgzt))    # 采购主体
+    pdf.drawString(65,694,str(project_name))    # 收货项目
+
+    pdf.drawString(220,727,str(sh_date)[:10])           # 送货日期
+
+
+
+
+
+
+
+
     for index in range(pages):
-        for i in range(min(len(data)-index*amount,amount)):
-            i = int(index*amount + i)    # 索引位置
+        pdf.setFont('song',25) # 设置字体
+
+        
+        # 加载图片 background
+        two_dimensional_code_path = L2['qr_code']
+        file_path = r"/home/webroot/data/lwerp/open/background/total.png"
+        pdf.drawImage(file_path,0,0,pageWidth*cm,pageHeight*cm)                 # 背景
+        pdf.drawImage(two_dimensional_code_path,17.61*cm,24.3*cm,2.82*cm,2.82*cm) # 二维码 
+
+        tel = L2['sup_tel']                   # 电话
+        usr_name = L2['usr_name']             # 制单人
+        cg_usr_name = L2['cg_usr_name']       # 采购员
+        shd_no = L2['shd_no']                 # 送货单号
+        cgzt   = L2['cgzt']                   # 采购主体
+        zt = L2['sup_name']                           # 送货单最上面大字
+        project_name = L2['proj_name']     # 收货项目
+        sh_date = L2['sh_date']                   # 送货日期
+        all_amount  = 0.                       # 合计金额
+
+
+        pdf.drawString(21,755,str(zt))    # 送货单最上面大字
+        pdf.setFont('song',9) # 设置字体  
+
+        pdf.drawString(135,21.5,str(cg_usr_name)) # 采购员
+        pdf.drawString(55,21.5,str(usr_name))    # 制单人
+        pdf.drawString(361,727,str(tel))    # 电话
+
+        pdf.drawString(65,727,str(shd_no))    # 送货单号
+        pdf.drawString(65,710.5,str(cgzt))    # 采购主体
+        pdf.drawString(65,694,str(project_name))    # 收货项目
+
+        pdf.drawString(220,727,str(sh_date)[:10])           # 送货日期
+
+
+
+
+
+
+
+
+        # ---------------   #
+        #   y坐标：Y + 100 = 上升 2.5格 -->1格：40
+        x = 32
+        x1, y = 48, 638 # x：材料名称的x1坐标 y:第一行记录初始y坐标
+        x2 = x1 +60
+        x3 = x2 + 90 # 规格
+        x4 = x3 + 70 # 型号
+        x5 = x4 +47 # 品牌
+        x6 = x5 + 40 # 单位
+        x7 = x6 + 22 # 数量
+        x8 = x7 + 50 # 单价
+        x9 = x8 + 37 # 金额
+        x10 = x9 + 51 # 备注
+        for i in range(min(len(data)-index*amounT,amounT)):
+            i = int(index*amounT + i)    # 索引位置
             print('i:',i)
             info = data[i]
             # 材料列表信息 
-            x, y = 15, 18
-            # for _ in info: 
-            serize = str(i+1)
+
+            serize = str(i+1)                       # 序号
             material_number = info['cl_code']       # 材料编码
+            # x,y = 800,900
             material_name = info['cl_name']         # 材料名称
             standard = info['cl_spec']              # 规格
             material_model = info['cl_model']       # 型号
+            material_model = 'WDZB-YJY'
             material_brand = info['cl_brand']       # 品牌
             material_unit = info['cl_unit']         # 单位
             quantity_current = info['total_qty']    # 数量
-            prince = info['price']                  # 数量
-            amount = info['amount']                     
-            remark = info['memo']
-            # x, y = x+20, y+18
-            pdf.drawString(x,y,material_number) # 处理
+            prince = info['price']                  # 单价
+            amount = info['amount']                 # 金额
+            remark = info['memo']                   # 备注
+            all_amount =all_amount + float(amount)    # 合计金额
+            drawText(x1+2,y,material_number,10)    # 写入材料编码
+            
+            pdf.drawString(x,y,serize) # 序号
+            # _t = '一二三四五六七八九'    # 9个汉字 =17 个字母
+            # 写入双行 18长度限制 3
+            drawText(x2,y,material_name,27)     # 材料名称
+            drawText(x3,y,standard,12)         # 规格
+            drawText(x4,y,material_model,11)    # 型号
+            drawText(x5,y,material_brand,9)     # 品牌
+            drawText(x6,y,material_unit,5)     # 单位
+            drawText(x7,y,quantity_current,5)     # 单位
+            drawText(x8,y,prince,5)     # 单价
+            drawText(x9,y,amount,25)     # 金额
+            drawText(x10,y,remark,19)     # 备注
+            y = y - 40  #  Y + 100 = 上升 2.5格 --1：40 
+        pdf.drawString(460,46,str(all_amount))          # 总额
+        pdf.drawString(115,46,str(process_all_amount(all_amount)))  # 大写
         pdf.showPage()
+
+
+    # 其他变量
+    # pdf.
     pdf.save()
     return response
 
+# 返回字节数
+def calLength(value):
+    length = len(value)
+    utf8_length = len(value.encode('utf-8'))
+    length = (utf8_length - length)//2 + length
+    return length
+
+# 多行绘制文本
+def drawText(x,y,V,length):
+    V = str(V)
+    global pdf
+    L = calLength(V)
+    _tempI = None
+    # pdf.drawString(200,200,str(L))
+    if 2*length> L > length:
+        for _tempI in range(3):
+            try:
+                pdf.drawString(x,y+5,V[:length+_tempI])
+                pdf.drawString(x,y-5,V[length+_tempI:])
+                break
+            except:
+                pass
+        else:
+            pdf.drawString(x,y-5,'eeror')
+
+    elif L>2*length:
+        for _tempI in range(3):
+            try:
+                pdf.drawString(x,y+5,V[:length+_tempI])
+                # pdf.drawString(x2,y-5,material_name[length+_tempI:])
+                # pdf.drawString(x2,y-5,material_name[-27+_tempI:])
+                break
+            except:
+                pass
+        drawText(x,y-10,V[length+_tempI:],length)
+        
+    else:
+        pdf.drawString(x,y,V)
+
+# 转换对应金额至中文
+def process_all_amount(value):
+    prefix={0:'零',1:'壹',2:'贰',3:'叁',4:'肆',
+        5:'伍',6:'陆',7:'柒',8:'捌',9:'玖'
+        }
+    suffix={0:'分',1:'角',2:'圆',3:'拾',4:'佰',
+        5:'仟',6:'万',7:'拾',8:'佰',9:'仟',10:'亿',11:'拾',12:'佰'
+        }
+    temp = [] # 转换容器
+    _value = str(value).replace('.','')
+    for i,v in enumerate(_value):
+        v = int(v)
+        if v%10:    # 不为0
+            temp.extend((prefix[v],suffix[len(_value)-i-1])) # 前缀+后缀
+        else:
+            if suffix[len(_value)-i-1] in ['圆','角','分']:
+                temp.append(suffix[len(_value)-i-1])
+            else:
+                temp.append(prefix[v])
+    return ''.join(temp)
