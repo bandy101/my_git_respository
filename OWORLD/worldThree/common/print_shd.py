eac824c8c55d73d57a4eb28749178b8aa42d3cb3
michaelwbzhu <1255381744@qq.com>
2019-03-13 18:35:32 +0800

    up

--- OWORLD/worldThree/common/print_shd.py
+++ OWORLD/worldThree/common/print_shd.py
@@ -15,7 +15,8 @@ import httplib
 from django.db import connection
 exec ('from %s.share import db,HttpResponseCORS,g_data,ToGBK,mValidateUser,fs_url,ComplexEncoder'%prj_name) 
 from qr_code import genShdCode
-
+from django.http import HttpResponse
+from django.http import Http404
 
 #
 from reportlab.pdfgen import canvas
@@ -33,7 +34,7 @@ def printShd(request):
              left join prj_mat_buy_ht ht on ht.id = cg.cght 
              left join mtc_t m1 on m1.type = 'cgzt' and m1.id = cg.cgzt
              left join mtc_t m2 on m2.type = 'cylgs' and m2.id = cg.gyl
-             where sh.id=%s
+             where sh.id=%s 
              """%(pk)
     print sql
     rows,iN = db.select(sql)
@@ -69,6 +70,18 @@ def printShd(request):
 
 def printPdfShd(request):
     pk =  request.POST.get('pk') or  request.GET.get('pk','')
+    flag =  request.POST.get('flag') or  request.GET.get('flag','') # 0:半 1 ：全张
+    token = request.POST.get('accessToken') or  request.GET.get('accessToken','')
+    # token = 'VURsNXBvQVZwR2k5eFpYS1Iva05SK2dXc21hV2xVa05GakkvdjdvTFJ0Zz0gMTU0MzM5MTcyNS44NyA5NzEwOTYgZWVhZDI4Mzg5MTU1MjAwMGEwZmM3NDU5NjBlYTI1MmY5OTYyMjU0ZQ=='
+    
+    _sql = """select usr_id from users_login_gy where token='%s'
+           """%(token)
+    print (_sql)
+    _t,_ = db.select(_sql)
+    print(_t)
+    if not _t:
+        raise Http404("没有权限访问")
+
     sql = """select op.cname,sh.sn,sh.ctime,su.cname,su.tel,sh.cusrname,ifnull(sh.qr_code,''),''
                    ,ht.req_no,cg.cusrname,m1.txt1,m2.txt1 
              from _m3000004_shd sh 
@@ -78,17 +91,17 @@ def printPdfShd(request):
              left join prj_mat_buy_ht ht on ht.id = cg.cght 
              left join mtc_t m1 on m1.type = 'cgzt' and m1.id = cg.cgzt
              left join mtc_t m2 on m2.type = 'cylgs' and m2.id = cg.gyl
-             where sh.id=%s
-             """%(pk)
+             where sh.id=%s and sh.cid=%s
+             """%(pk,_t[-1][-1])
     # print sql
     rows,iN = db.select(sql)
     if iN == 0:
         s = """
         {
-        "errcode": -1,
-        "errmsg": "获取送货单数据失败"
+            本张送货单不存在或您没有查看权限！
         }        """
-        return HttpResponseCORS(request,s)
+        # return HttpResponseCORS(request,s)
+        raise Http404(s)
     L = list(rows[0])
     if L[6] == '':
         L[6] = genShdCode(pk)
@@ -104,10 +117,17 @@ def printPdfShd(request):
 
     data = L2['lists'] # 材料明细内容数据
     #print ToGBK(s)
-
-    # 页面大小   (四联二分217*140mm ，四联 217*280mm: 4.4 2.72)
+    
+    # 页面大小   (四联二分217*140mm ，四联 217*280mm)
     pageWidth = 21.7     
     pageHeight = 28.0
+    amounT = 15 # 每页显示的数目 (四联二等分:5, 四联整张纸:15)
+    if flag in [0,'0']:
+        amounT = 5                     
+    imgName = 'total'
+    if amounT-15:
+        pageWidth, pageHeight= 21.7, 14.0
+        imgName = 'half'
     response = HttpResponse(content_type='application/pdf')   
     global pdf
     pdf = canvas.Canvas(response) # 实例化pdf
@@ -117,16 +137,15 @@ def printPdfShd(request):
     # 1 inch = 2.54cm  1cm = 28.346 dpi
     pdf.setPageSize((pageWidth*cm,pageHeight*cm)) # 尺寸
 
-    amounT = 15                     # 每页显示的数目 (四联二等分:5, 四联整张纸:15)
     pages = len(data)//amounT + 1         # 页数
     for index in range(pages):
         pdf.setFont('song',25) # 设置标题字体
         
         # 加载图片 background
         two_dimensional_code_path = L2['qr_code']   # 二维码
-        file_path = r"/home/webroot/data/lwerp/open/background/total.png"
+        file_path = r"/home/webroot/data/lwerp/open/background/"+str(imgName)+".png"
         pdf.drawImage(file_path,0,0,pageWidth*cm,pageHeight*cm)                 # 背景
-        pdf.drawImage(two_dimensional_code_path,17.61*cm,24.3*cm,2.82*cm,2.82*cm) # 二维码 
+        pdf.drawImage(two_dimensional_code_path,17.61*cm,24.3*cm-(15-amounT)*40,2.82*cm,2.82*cm) # 二维码 
 
         tel = L2['sup_tel']                   # 电话
         usr_name = L2['usr_name']             # 制单人
@@ -137,24 +156,27 @@ def printPdfShd(request):
         project_name = L2['proj_name']     # 收货项目
         sh_date = L2['sh_date']                   # 送货日期
         all_amount  = 0.                       # 合计金额
-
-        pdf.drawString(21,755,str(zt))    # 送货单最上面大字
+        
+        pdf.drawString(21,755-(15-amounT)*40,str(zt))    # 送货单最上面大字
         pdf.setFont('song',9) # 设置文字字体  
 
         pdf.drawString(135,21.5,str(cg_usr_name)) # 采购员
         pdf.drawString(55,21.5,str(usr_name))    # 制单人
-        pdf.drawString(361,727,str(tel))    # 电话
+        #  页码
 
-        pdf.drawString(65,727,str(shd_no))    # 送货单号
-        pdf.drawString(65,710.5,str(cgzt))    # 采购主体
-        pdf.drawString(65,694,str(project_name))    # 收货项目
+        pdf.drawString(65,727-(15-amounT)*39.65,str(shd_no))    # 送货单号
+        pdf.drawString(65,711-(15-amounT)*39.65,str(cgzt))    # 采购主体
+        pdf.drawString(65,694-(15-amounT)*39.65,str(project_name))    # 收货项目
 
-        pdf.drawString(220,727,str(sh_date)[:10])           # 送货日期
-
+        pdf.drawString(220,727-(15-amounT)*39.65,str(sh_date)[:10])           # 送货(开单)日期
+        pdf.drawString(361,727-(15-amounT)*39.65,str(tel))    # 电话
+        # 购买方式
+        buyWay = '赊购'.encode('utf-8')
+        pdf.drawString(335,711-(15-amounT)*39.65,buyWay)
         # ---------------   #
         #   y坐标：Y + 100 = 上升 2.5格 -->1格：40
         x = 32  # 序号
-        x1, y = 48, 638 # x：材料名称的x1坐标 y:第一行记录初始y坐标
+        x1, y = 48, 638-(15-amounT)*40 # x：材料名称的x1坐标 y:第一行记录初始y坐标
         x2 = x1 +60     # 材料名称
         x3 = x2 + 90 # 规格
         x4 = x3 + 70 # 型号
@@ -187,7 +209,7 @@ def printPdfShd(request):
             pdf.drawString(x,y,serize) # 序号
             # _t = '一二三四五六七八九'    # 9个汉字 =17 个字母
             # 写入双行 18长度限制 3
-            drawText(x2,y,material_name,27)     # 材料名称
+            drawText(x2,y,material_name,17)     # 材料名称
             drawText(x3,y,standard,12)         # 规格
             drawText(x4,y,material_model,11)    # 型号
             drawText(x5,y,material_brand,9)     # 品牌
@@ -199,6 +221,7 @@ def printPdfShd(request):
             y = y - 40  #  Y + 100 = 上升 2.5格 --1：40 
         pdf.drawString(460,46,str(all_amount))          # 总额
         pdf.drawString(115,46,str(process_all_amount(all_amount)))  # 大写
+        pdf.drawString(542,21.5,"{}/{}".format(index+1,pages))                              # 页码
         pdf.showPage()
 
     # 其他变量
@@ -262,6 +285,7 @@ def process_all_amount(value):
             if suffix[len(_value)-i-1] in ['圆',] and int(float(value)):
                 temp.append(suffix[len(_value)-i-1])
             else:
-                if suffix[len(_value)-i-1] not in ['分','角'] and temp[-1]!='零':
-                    temp.append(prefix[v])
+                if suffix[len(_value)-i-1] not in ['分','角'] and temp:
+                    if temp[-1]!='零':
+                        temp.append(prefix[v])
     return ''.join(temp)
