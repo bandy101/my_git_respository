05eeebe940ff1e50cabdc9c7ccd4560e68826372
michaelwbzhu <1255381744@qq.com>
2019-03-14 18:26:50 +0800

    update

---
+++ farAwaySFE/mine/django/oworld/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+# admin.site.register(Post)
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ( 'title', 'slug', 'author', 'body','publish','status',) # list_display属性指定那些字段在详情页中显示出来
+    list_filter = ('status', 'created', 'publish', 'author',)
+    date_hierarchy = 'publish'
