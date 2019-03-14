a48a8e7fbbe73540c462f27ce21fbb44909ca127
michaelwbzhu <1255381744@qq.com>
2019-03-14 18:37:09 +0800

    update

--- farAwaySFE/mine/django/oworld/blog/admin.py
+++ farAwaySFE/mine/django/oworld/blog/admin.py
@@ -9,3 +9,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ( 'title', 'slug', 'author', 'body','publish','status',) # list_display属性指定那些字段在详情页中显示出来
     list_filter = ('status', 'created', 'publish', 'author',)
     date_hierarchy = 'publish'
+    search_fields = ('title', 'body',)
+    raw_id_fields = ('author',)
+    prepopulated_fields = {'slug': ('title',)}
